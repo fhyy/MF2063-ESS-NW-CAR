a98ecc06e6ed1382c8482ec03144d03b57898457
jacobki <jacobki@kth.se>
2018-10-30 13:24:03 +0100

    Fix erroneous speed measurements

--- ard-speed/ard-speed.ino
+++ ard-speed/ard-speed.ino
@@ -8,26 +8,25 @@
 //      AFL 3.0 
 //
 
-#include "SimpleTimer.h"
-// Library installed from:
-// https://playground.arduino.cc/code/SimpleTimer
+#include "Time.h"
+#include "TimeAlarms.h"
+// Libraries isntalled from
+// https://www.pjrc.com/teensy/td_libs.html
 
 //Constants
 int const MAX_STEPS = 5;
-int const TIMEOUT_TIME = 10000;
+int const TIMEOUT_TIME = 1000;
 int const WHEEL_RADIUS_MM = 55;
 int const WHEEL_CIRCUMFERENCE = 2 * PI * WHEEL_RADIUS_MM;
 int const STEPS_PER_ROTATION = 5;
-int const DISTANCE_PER_STEP = WHEEL_CIRCUMFERENCE / STEPS_PER_ROTATION;
+float const DISTANCE_PER_STEP = (float)WHEEL_CIRCUMFERENCE / (float)STEPS_PER_ROTATION;
 //Pins
-int const IR_PIN = 22;
+int const IR_PIN = 2;
 //Global variables
 bool timeoutFlag = false;
 int steps = 0; 
 // Pin readings
 int irVal = 0;      //Int to store IR-value
-//Timers
-SimpleTimer timer1;
 
 void setup() {
     //Init serial communication to enable prints
@@ -44,21 +43,25 @@ void setup() {
 
 // put your main code here, to run repeatedly:
 void loop() {
-    //Start timeout countdown until next measurment update
-    int timerID = timer1.setTimeout(TIMEOUT_TIME, setTimeoutFlag);
-    Serial.println(timer1.getNumTimers());
 
     // Start time-measurments
     unsigned long startTime = millis();
 
     //Wait until sufficient data is captured
-    while(steps < MAX_STEPS && !timeoutFlag); 
+    while(steps < MAX_STEPS && !timeoutFlag) {
+        if (millis() - startTime > TIMEOUT_TIME) {
+            setTimeoutFlag();
+        }
+        //Check if 1s of waiting has passed and call setTimeoutFLag
+        //Checks state of timeout timer
+    } 
 
     // Stop time-measurements
     unsigned long stopTime = millis();
     unsigned long elapsedTime = stopTime - startTime;
 
-    int speed = calculateSpeed(elapsedTime);
+    // Calculate actual speed
+    float speed = calculateSpeed(elapsedTime);
 
     //Reset flags before next loop
     timeoutFlag = false;
@@ -67,7 +70,7 @@ void loop() {
 
 // Callback-function from timer
 void setTimeoutFlag(){
-   timeoutFlag = true;
+    timeoutFlag = true;
 }
 
 // IR-sensor interrupt handler
@@ -76,13 +79,10 @@ void irInterruptHandler(){
 }
 
 // Calculate speed using elapsed time and distance traveled
-int calculateSpeed(unsigned long elapsedTime){
-    // distance traveled = steps * WHEEL_CIRCUMFERENCE / STEPS_PER_ROTATION
+float calculateSpeed(unsigned long elapsedTime){
     int distance = steps * DISTANCE_PER_STEP; //Gives distance in millimeters
-    float speedMMPerMS = distance/(elapsedTime); //Gives distance/time (mm per ms)
-    //Uncomment to calculate meters per second
-    /*unsigned float speedMPerS = speedMMPerMS * 1000; //Gives speed in meters per second*/
+    float speedMMPerS = (float)(distance*1000)/(float)elapsedTime; //Gives distance*1000/time (mm per s)
 
-    return speedMMPerMS;
+    return speedMMPerS;
 }
 
