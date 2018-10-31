fe2bc161a334d0b2f0cd024f5398250d1f3e0044
jacobki <jacobki@kth.se>
2018-10-31 10:55:54 +0100

    Transfer information over SPI

--- ard-speed/ard-speed.ino
+++ ard-speed/ard-speed.ino
@@ -8,10 +8,7 @@
 //      AFL 3.0 
 //
 
-#include "Time.h"
-#include "TimeAlarms.h"
-// Libraries isntalled from
-// https://www.pjrc.com/teensy/td_libs.html
+#include <SPI.h>
 
 //Constants
 int const MAX_STEPS = 5;
@@ -25,25 +22,26 @@ int const IR_PIN = 2;
 //Global variables
 bool timeoutFlag = false;
 int steps = 0; 
+float speed = 0.0;
 // Pin readings
 int irVal = 0;      //Int to store IR-value
 
-void setup() {
+void setup() 
+{
     //Init serial communication to enable prints
     Serial.begin(115200);
 
-    //Try printing
-    Serial.println("Serial communication set up!");
+    //Setup spi
+    SPI.begin();
 
     //Setup IR-input to interrupt mcu
     pinMode(IR_PIN, INPUT);
     attachInterrupt(digitalPinToInterrupt(IR_PIN), irInterruptHandler, RISING);
-
 }
 
 // put your main code here, to run repeatedly:
-void loop() {
-
+void loop() 
+{
     // Start time-measurments
     unsigned long startTime = millis();
 
@@ -52,8 +50,6 @@ void loop() {
         if (millis() - startTime > TIMEOUT_TIME) {
             setTimeoutFlag();
         }
-        //Check if 1s of waiting has passed and call setTimeoutFLag
-        //Checks state of timeout timer
     } 
 
     // Stop time-measurements
@@ -61,7 +57,10 @@ void loop() {
     unsigned long elapsedTime = stopTime - startTime;
 
     // Calculate actual speed
-    float speed = calculateSpeed(elapsedTime);
+    speed = calculateSpeed(elapsedTime);
+
+    //Send speed value over SPI
+    SPI.transfer(speed);
 
     //Reset flags before next loop
     timeoutFlag = false;
@@ -69,17 +68,20 @@ void loop() {
 }
 
 // Callback-function from timer
-void setTimeoutFlag(){
+void setTimeoutFlag()
+{
     timeoutFlag = true;
 }
 
 // IR-sensor interrupt handler
-void irInterruptHandler(){
+void irInterruptHandler()
+{
     steps++;
 }
 
 // Calculate speed using elapsed time and distance traveled
-float calculateSpeed(unsigned long elapsedTime){
+float calculateSpeed(unsigned long elapsedTime)
+{
     int distance = steps * DISTANCE_PER_STEP; //Gives distance in millimeters
     float speedMMPerS = (float)(distance*1000)/(float)elapsedTime; //Gives distance*1000/time (mm per s)
 
