ee7fe2b48430270a1b90ea61b9e91a5a48b52d5e
ruanyifan <ruanyfee1@gmail.com>
2018-11-12 10:23:48 +0100

    update multiBytes code

--- bbb-distance-steering/distance.cpp
+++ bbb-distance-steering/distance.cpp
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <sys/ioctl.h>
 #include <stdint.h>
 #include <linux/spi/spidev.h>
+#include <time.h>
+#include <sys/time.h>
 
 #define SPI_PATH "/dev/spidev1.0"
 
 int main(){
         unsigned int fd,i;
-        char a;
+        char  a[3],b;
         uint8_t bits=8, mode=0;
-        uint32_t speed=10000;
+        uint32_t speed=1000000;
         int distance;
-        uint8_t rx[4],tx[4];
+        uint8_t rx[2]={0},tx[2]={0};
+        double begin,end;
+        struct timeval tv;
+
         struct spi_ioc_transfer tr;
         fd = open(SPI_PATH, O_RDWR);
         ioctl(fd, SPI_IOC_WR_MODE, &mode);
@@ -23,25 +31,17 @@ int main(){
         ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
         ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
 
-        tr.tx_buf = (unsigned long)tx;
-        tr.rx_buf = (unsigned long)rx;
-        tr.len = 1;
-        tr.speed_hz = 10000000;
-        tr.bits_per_word = 8;
-        tr.delay_usecs = 0;
-        tr.rx_buf = (unsigned long)rx;
-        tr.len = 1;
-        tr.speed_hz = 10000000;
-        tr.bits_per_word = 8;
-        tr.delay_usecs = 0;
-        for(i=0;i<20;i++){
-                read(fd,&a,1);
-                printf("%d\n\n",a);
-                fflush(stdout);
-                usleep(100000);
+         for(i=0;i<20;i++){
+                read(fd,&b,1);
+                if(b==0x0){
+                        read(fd,&a,3);
+                        printf("%d %d %d\n",a[0],a[1], a[2]);
+                        fflush(stdout);
+                        usleep(100000);
+                }else
+                        break;
         }
         close(fd);
         return 0;
 }
 
-    
