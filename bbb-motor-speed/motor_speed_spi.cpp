c48e7af88a77e4870e557bf82e9f6c620e364713
jacobki <jacobki@kth.se>
2018-11-22 13:29:20 +0100

    Create process to handle SPI communication

---
+++ bbb-motor-speed/motor_speed_spi.cpp
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdint.h>
+#include <linux/spi/spidev.h>
+#include <time.h>
+#include <sys/time.h>
+#include "SimpleGPIO.h"
+
+#define SPI_PATH "/dev/spidev1.0"
+#define EVER (;;)
+#define MOTOR_PIN 60
+#define SENSOR_PIN 48
+
+//Initialize a GPIO-pin to act as a Slave Select (SS)
+void spiSSInit(int gpio)
+{
+        gpio_export(gpio);
+        gpio_set_dir(gpio, OUTPUT_PIN);
+        gpio_set_value(gpio, HIGH);
+}
+
+// SPI parameter setup, takes a file descriptor to the SPI interface
+void spiInit(unsigned int fd)
+{
+        //GPIO-Pin 60 (P9-12) is slave select for the motor controller
+        //GPIO-Pin 48 (P9-15) is slave select for the speed sensor
+        //Initiates all slave-select pins so we can set them manually to the chip we want to communicate with
+        spiSSInit(MOTOR_PIN);   //pin 60
+        spiSSInit(SENSOR_PIN);  //Pin 48
+        
+        //SPI parameters
+        uint8_t bits=8, mode=0;
+        uint32_t speed=1000000;
+        //Spi parameter setup
+        ioctl(fd, SPI_IOC_WR_MODE, &mode);
+        ioctl(fd, SPI_IOC_RD_MODE, &mode);
+        ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+        ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
+        ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+        ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+}
+
+//To start spi communication we set the correct SlaveSelect gpio-pin to low
+void spiStart(int gpio)
+{
+        gpio_set_value(gpio, LOW);
+}
+
+//To stop spi communication we set the correct SlaveSelect gpio-pin to high
+void spiStop(int gpio)
+{
+        gpio_set_value(gpio, HIGH);
+}
+
+//We send a given value to the motor controller arduino over spi
+void sendMotorValue(unsigned int fd, unsigned char controlValue)
+{
+        spiStart(MOTOR_PIN);
+        write(fd, &controlValue, 1);
+        spiStop(MOTOR_PIN);
+}
+
+//We read a single speed value from the given arduino over spi
+int readSensorValue(unsigned int fd)
+{
+        int received = 0;
+        // Talk to the speed sensor by enabling its slave select (active low)
+        spiStart(SENSOR_PIN);
+        //Read received value pointed to by the file descriptor
+        read(fd, &received, 1);
+        //stop SPI transfer
+        spiStop(SENSOR_PIN);
+
+        return received;
+}
+
+//This program intermediately reads a speed value from the speed sensor and sends a desired control variable to the motor controller. The desired control variable can be in terms of distance or speed depending on the implementation of the car controller which is implemented on another beaglebone. The communication between the arduinos and this beaglebone is done over SPI.
+int main(void)
+{
+        unsigned int fd,i;
+        char receivedMessage;
+
+        fd = open(SPI_PATH, O_RDWR);
+        // SPI parameter setup
+        spiInit(fd);
+
+        //Endless loop
+        for EVER {
+
+                //Read the sensor value given by the spedometer
+                receivedMessage = readSensorValue(fd);
+
+                //TODO: write to the named pipe to send the information over vsomeip
+
+                //Print received value for debugging
+                if(receivedMessage != 0) {
+                    printf("%d \n", receivedMessage);
+                }
+
+                //TODO: get values from the named pipe to send to the motor controller
+                int controlValue = 30;
+
+                //Send the values to the arduino motor controller
+                sendMotorValue(fd, controlValue);
+
+                //We sleep so we dont interrupt the arduino to often and it never gets to do work
+                usleep(50000);
+        }
+
+        return 1;
+}
+
+
