80373917907a4759602ed5483da394af9a316765
zluudg <zluudg@zluudg.xyz>
2018-11-25 13:49:23 +0100

    Changes password of user debian

--- bbb-motor-speed/linux_configurations/cfg_bbb.sh
+++ bbb-motor-speed/linux_configurations/cfg_bbb.sh
@@ -6,4 +6,4 @@ sudo cp interfaces /etc/network/interfaces
 sudo chattr +i /etc/network/interfaces #prevent other programs from modifying
 sudo cp resolv.conf /etc/resolv.conf
 sudo chattr +i /etc/resolv.conf #prevent other programs from modifying
-
+sudo echo -e "mf2063\nmf2063" | passwd debian
